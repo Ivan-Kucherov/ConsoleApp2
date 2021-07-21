5649228119c4523a25ea9d7598b089d9d5ca68d1
XOPC <XOPC@DESKTOP-TODR3GR>
2021-07-21 18:58:33 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Data;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days days = new Days();
+            int c;
+            DataTable data;
+            DataView view;
+            while (true)
+            {
+                Console.Write("1.Вывод списка дней рождения\n" +
+                    "2.Вывод 3 ближайших дней рождения\n" +
+                    "3.Добавление в список\n" +
+                    "4.Удаление из списка\n" +
+                    "5.Изменение записи\n");
+                c = Convert.ToInt32(Console.ReadLine());
+                if (c == 1)
+                {
+                    data = days.GetData();
+                    Console.WriteLine("id   Имя           Дата рождения      Заметки");
+                    foreach (DataRow item in data.Rows)
+                    {
+                        Console.WriteLine(item[0].ToString() + "  " + item[1].ToString() + ((DateTime)item[2]).ToShortDateString() + "       " + item[3].ToString());
+                    }
+                }
+                if (c == 2)
+                {
+                    data = days.GetDataLast();
+                    view = data.DefaultView;
+                    Console.WriteLine("Ближайшие дни рождения в этом году: ");
+                    Console.WriteLine("id   Имя           День рождения      Заметки");
+                    if(view.Count >0)
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (view[i] == null)
+                            break;
+                        Console.WriteLine(view[i][0].ToString() + "  " + view[i][1].ToString() + ((DateTime)view[i][2]).ToShortDateString() + "       " + view[i][3].ToString());
+                    }
+                }
+                if (c == 3)
+                {
+                    Console.WriteLine("Введите имя");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Введите дату рождения");
+                    DateTime date;
+                    while (true)
+                    {
+                        try
+                        {
+                            date = Convert.ToDateTime(Console.ReadLine());
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Неверный ввод, повторите");
+                        }
+                    }
+                    Console.WriteLine("Введите доп. информацию");
+                    string note = Console.ReadLine();
+                    if (!(days.add(name, date, note)))
+                        Console.WriteLine("Ошибка");
+                }
+                if (c == 4)
+                {
+                    data = days.GetData();
+                    Console.WriteLine("id   Имя           Дата рождения      Заметки");
+                    foreach (DataRow item in data.Rows)
+                    {
+                        Console.WriteLine(item[0].ToString() + "  " + item[1].ToString() + ((DateTime)item[2]).ToShortDateString() + "       " + item[3].ToString());
+                    }
+                    Console.WriteLine("Введите id для удаления");
+                    int id;
+                    while (true)
+                    {
+                        try
+                        {
+                            id = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Неверный ввод, повторите");
+                        }
+                    }
+                    if (!(days.delete(id)))
+                        Console.WriteLine("Ошибка");
+                }
+                if (c == 5)
+                {
+                    data = days.GetData();
+                    Console.WriteLine("id   Имя           Дата рождения      Заметки");
+                    foreach (DataRow item in data.Rows)
+                    {
+                        Console.WriteLine(item[0].ToString() + "  " + item[1].ToString() + ((DateTime)item[2]).ToShortDateString() + "       " + item[3].ToString());
+                    }
+                    Console.WriteLine("Введите id для изменения(Поля без введенных данных останутся без изменений)");
+                    int id;
+                    DataRow row = null;
+                    while (true)
+                    {
+                        try
+                        {
+                            id = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Неверный ввод, повторите");
+                        }
+                    }
+                    foreach (DataRow item in data.Rows)
+                    {
+                        if ((int)item[0] == id)
+                        {
+                            row = item;
+                            break;
+                        }
+                    }
+                    if (row != null)
+                    {
+                        Console.WriteLine(row[0].ToString() + "  " + row[1].ToString() + ((DateTime)row[2]).ToShortDateString() + "       " + row[3].ToString());
+                        Console.WriteLine("Введите имя");
+                        string name = Console.ReadLine();
+                        if (name == "")
+                        {
+                            name = row[1].ToString();
+                        }
+                        Console.WriteLine("Введите дату рождения");
+                        DateTime date;
+                        while (true)
+                        {
+                            try
+                            {
+                                string dates = Console.ReadLine();
+                                if (dates == "")
+                                {
+                                    date = Convert.ToDateTime(row[2]);
+                                    break;
+                                }
+                                date = Convert.ToDateTime(dates);
+                                break;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Неверный ввод, повторите");
+                                throw;
+                            }
+                        }
+                        Console.WriteLine("Введите доп. информацию");
+                        string note = Console.ReadLine();
+                        if (note == "")
+                        {
+                            note = row[3].ToString();
+                        }
+                        if (!(days.change(id, name, date, note)))
+                            Console.WriteLine("Ошибка");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не найдено");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/days.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp2
+{
+    public class Days
+    {
+        private static string strConnect = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={Environment.CurrentDirectory}\Database1.mdf;Integrated Security=True";
+        private SqlConnection connect;
+        private SqlDataAdapter adapter;
+        private SqlCommand command;
+        private DataTable datatable;
+        public Days()
+        {
+            connect = new SqlConnection(strConnect);
+            datatable = new DataTable();
+        }
+        public DataTable GetData()
+        {
+            connect.Open();
+            datatable.Clear();
+            adapter = new SqlDataAdapter("SELECT * FROM Table1", connect);
+            adapter.Fill(datatable);
+            connect.Close();
+            return datatable;
+        }
+        public DataTable GetDataLast()
+        {
+            connect.Open();
+            datatable.Clear();
+            adapter = new SqlDataAdapter($"SELECT * FROM Table1 WHERE month(Date)>= {DateTime.Today.Month} ORDER BY month(Date)*100+day(Date) ASC", connect);
+            adapter.Fill(datatable);
+            connect.Close();
+            return datatable;
+        }
+        public bool delete(int id)
+        {
+            bool end;
+            connect.Open();
+            command = new SqlCommand($"DELETE FROM Table1 WHERE id = {id}", connect);
+            end =  (command.ExecuteNonQuery() >0);
+            connect.Close();
+            return end;
+        }
+        public bool add(string name,DateTime date,string note)
+        {
+            bool end;
+            connect.Open();
+            command = new SqlCommand($"INSERT INTO Table1 (Name, Date, Note) VALUES ('{name}', '{date.ToString("yyyy-MM-dd")}', '{note}') ", connect);
+            end = (command.ExecuteNonQuery() > 0);
+            connect.Close();
+            return end;
+        }
+
+        public bool change(int id, string name, DateTime date, string note)
+        {
+            bool end;
+            connect.Open();
+            command = new SqlCommand($"UPDATE Table1 SET Name = '{name}', Date = '{date.ToString("yyyy-MM-dd")}', Note = '{note}' WHERE id = {id}", connect);
+            end = (command.ExecuteNonQuery() > 0);
+            connect.Close();
+            return end;
+        }
+    }
+}
